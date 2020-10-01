c750d9e9128e7e17c9547ebcd480ca95669f9593
jaba17 <jakob.bach15@gmail.com>
2020-10-01 23:30:47 +0200

    Add list, execute and Add Functionality

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Command_DB
 A simple CLI tool which manages your favorite Linux commands.
+
+
+# How to install: 
+In order to install you have to execute the ./setup file. 
+But first you have to make the file executable by typing ```sudo chmod +x setup```
+
+*More Installation infos coming soon.*
+
+
+# How to use: 
+
+## Add a new command to the DB: 
+In order to add a new command you have to give it a unique id, eg "list_files" for the command "ls -la". 
+```
+# ./command_db.py add "list_files" "ls -la"
+```
+
+Furthermore, you can add a description of the command if you want. 
+```
+# ./command_db.py add "list_files" "ls -la" -d "This command lists all files in the CD."
+```
+
+## Remove a command: 
+*TODO*
+
+
+## List all commands: 
+This prints out all saved commands in a list. 
+```
+# ./command_db.py list
+```
+
+## Execute a specific command
+```
+# ./command_db.py execute "list_files"
+```
--- command_db.py
+++ command_db.py
@@ -1,38 +1,62 @@
+#!/usr/bin/python3
+
 import json
 import os
+import click
+
+
+# Main Function
+@click.group()
+def cli():
+	pass
+
 
 # Print out all saved commands
-def list_commands():
+@cli.command()
+def list():
 	json_data = json.loads(open('commands.json').read())
-	print("Saved Commands:")
-	print('------------------------------------------------------------------------')
-
+	click.echo("Saved Commands:")
+	click.echo('------------------------------------------------------------------------')
 	for command in json_data['commands']:
-		print("Shortcut: " + command[0])
-		print("Description: " + command[1])
-		print("Command: " + command[2])
-		print('------------------------------------------------------------------------')
-
+		click.echo("Shortcut: " + command[0])
+		click.echo("Description: " + command[1])
+		click.echo("Command: " + command[2])
+		click.echo('------------------------------------------------------------------------')
 
 
 # Add a new command to dataset
-def add_command(shortcut, description, command):
-
+@cli.command()
+@click.option('--d',  help='Description', default='')
+@click.argument('args', nargs=2)
+def add(args, d):
+	command = args[0]
+	shortcut = args[1]
 	json_data = json.loads(open('commands.json').read())
+	commands = json_data['commands']
+	print(commands)
 
-	json_data['commands'].append([shortcut, description, command])
+	if any(shortcut in x[0] for x in commands):
+		click.echo("Error: Shortcut already used.")
+
+	if any(shortcut in x[2] for x in commands):
+		click.echo("Error: Command already saved.")
+
+		
+	else: 
+		json_data['commands'].append([shortcut, d, command])
+
+		with open('commands.json', 'w') as writer:
+
+			writer.write(json.dumps(json_data))
+			writer.close()
+
+		click.echo("Added '{}' to dataset.".format(shortcut))
 
 
-
-	with open('commands.json', 'w') as writer:
-		writer.write(json.dumps(json_data))
-		writer.close()
-
-
-	print("Added '{}' to dataset.".format(shortcut))
-
-
-def get_command(shortcut):
+# Get the command and execute it
+@click.argument('shortcut')
+@cli.command()
+def execute(shortcut):
 	json_data = json.loads(open('commands.json').read())
 
 	ret_command = "echo 'Error in Command_Save'"
@@ -42,9 +66,10 @@ def get_command(shortcut):
 
 			ret_command = command[2]
 			break
-
+	click.echo("Executing '{}'.".format(shortcut))
 	os.system(ret_command)
-get_command("show_file");
 
-#get_command()
-#add_command("show_file", "shows files", "ls") 
+
+# Start cli() function
+if __name__ == '__main__':
+    cli()
